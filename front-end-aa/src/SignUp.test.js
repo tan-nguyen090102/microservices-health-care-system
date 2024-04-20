21065e176132fb6cafb3c09d8ba2a81727840c2a
amytrinh <amy-trinh@uiowa.edu>
2024-04-19 21:58:40 -0500

    Add new file

---
+++ front-end-aa/src/SignUp.test.js
@@ -0,0 +1,118 @@
+import "@testing-library/jest-dom";
+import { act, cleanup, render, screen, waitFor } from "@testing-library/react";
+import SignUpPanel from "./SignUp";
+import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { testObject } from "./testUser";
+import React from "react";
+
+//Clean up after each test
+afterEach(() => {
+    cleanup();
+})
+
+//Mock fetch
+global.fetch = jest.fn(() => 
+    Promise.resolve({
+        json: () => Promise.resolve({ data: "mock" }),
+    })
+);
+
+let spy = jest.SpyInstance;
+beforeEach(() => {
+    spy = jest.spyOn(console, "error").mockImplementation(() => null);
+})
+afterEach(() => {
+    spy.mockRestore();
+});
+
+test("first name input", async () => {
+    render(
+        <BrowserRouter>
+            <SignUpPanel />
+        </BrowserRouter>
+    );
+    await act(() => {
+        const firstnameInputField = screen.getByTestId("firstName");
+        userEvent.type(firstnameInputField, testObject.firstname);
+        expect(firstnameInputField).toHaveValue(testObject.firstname);
+    });
+});
+
+test("last name input", async () => {
+    render(
+        <BrowserRouter>
+            <SignUpPanel />
+        </BrowserRouter>
+    );
+    await act(() => {
+        const lastnameInputField = screen.getByTestId("lastName");
+        userEvent.type(lastnameInputField, testObject.lastname);
+        expect(lastnameInputField).toHaveValue(testObject.lastname);
+    });
+})
+
+test("email", async () => {
+    render(
+        <BrowserRouter>
+            <SignUpPanel />
+        </BrowserRouter>
+    );
+    await act(() => {
+        const emailInputField = screen.getByTestId("email");
+        userEvent.type(emailInputField, testObject.email);
+        expect(emailInputField).toHaveValue(testObject.email);
+    });
+})
+
+test("password", async () => {
+    render(
+        <BrowserRouter>
+            <SignUpPanel />
+        </BrowserRouter>
+    );
+    await act(() => {
+        const passwordInputField = screen.getByTestId("password");
+        userEvent.type(passwordInputField, testObject.password);
+        expect(passwordInputField).toHaveValue(testObject.password);
+    });
+})
+
+test("Signup button click", async () => {
+    render(
+        <BrowserRouter>
+            <SignUpPanel />
+        </BrowserRouter>
+    );
+    await act(() => {
+        const submitButton = screen.getByTestId("submitButton");
+        act(() => userEvent.click(submitButton));
+        waitFor(() => expect(submitButton).toBeDefined());
+    })
+})
+
+test("login link", async () => {
+    render(
+        <BrowserRouter>
+            <SignUpPanel />
+        </BrowserRouter>
+    );
+    await act(() => {
+        const loginlink = screen.getByTestId("loginlink");
+        act(() => userEvent.click(loginlink));
+        waitFor(() => expect(loginlink).toBeDefined());
+    })
+})
+
+test("forgot link", async () => {
+    render(
+        <BrowserRouter>
+            <SignUpPanel />
+        </BrowserRouter>
+    );
+    await act(() => {
+        const resetlink = screen.getByTestId("forgotlink");
+        act(() => userEvent.click(resetlink));
+        waitFor(() => expect(resetlink).toBeDefined());
+    })
+})
