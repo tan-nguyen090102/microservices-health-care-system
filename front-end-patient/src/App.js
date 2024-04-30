166c336cf4fd9182374b0ba85ec02a7b1fa8afba
amytrinh <amy-trinh@uiowa.edu>
2024-04-30 15:17:56 -0500

    Update App.js

--- front-end-patient/src/App.js
+++ front-end-patient/src/App.js
@@ -1,11 +1,11 @@
 import "react-router-dom";
 import { Route, Routes} from "react-router-dom";
 import HomePanel from "./HomePatient";
-import Visits from "./Visits";
 import Messages from "./Messages";
 import TestResults from "./TestResults";
 import Medications from "./Medications";
 import PatientDashboard from "./PatientDashboard";
+import PatientInfoSurvey from "./PatientInfoSurvey";
 
 function App() {
   return (
@@ -13,10 +13,10 @@ function App() {
       <PatientDashboard />
         <Routes>
           <Route path="/patient-home" element={<HomePanel></HomePanel>}></Route>
-          <Route path="/visits" element={<Visits></Visits>}></Route>
           <Route path="/messages" element={<Messages></Messages>}></Route>
           <Route path="/test-results" element={<TestResults></TestResults>}></Route>
           <Route path="/medications" element={<Medications></Medications>}></Route>
+          <Route path="/patient-info-survey" element={<PatientInfoSurvey></PatientInfoSurvey>}></Route>
         </Routes>
     </div>  
   );
