5d12ba4310d3c2d5f359c89ebeae44cabb97f0b4
amytrinh <amy-trinh@uiowa.edu>
2024-04-30 15:18:33 -0500

    Update HomePatient.js

--- front-end-patient/src/HomePatient.js
+++ front-end-patient/src/HomePatient.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { 
   Flex, 
@@ -6,7 +6,6 @@ import {
   Heading, 
   Text, 
   Wrap,
-  Spacer,
   Box
 } from "@chakra-ui/react";
 
@@ -37,7 +36,17 @@ export default function HomePanel() {
           console.log(data);
           if (data[3] === "P") {
             setUnauthorizedAccess(false);
-            console.log("Welcome " + data[1]);
+            const userID = data[1];
+            Promise.all([
+              fetchPatientVisits(userID),
+              fetchPatientInfo(userID),
+              console.log("Welcome " + data[1])
+            ])
+            .then(([visitsData, infoData]) => {
+              console.log("Patient Visits:", visitsData);
+              console.log("Patient Info:", infoData);
+              console.log("Welcome " + data[1]);
+            })
           } else {
             setUnauthorizedAccess(true);
           }
@@ -66,6 +75,47 @@ export default function HomePanel() {
       });
   };
 
+
+  const [patientInfo, setPatientInfo] = useState(null);
+  const fetchPatientInfo = (userID) => {
+    console.log(userID)
+    fetch(`http://${loginIP}:5000/patient-info-survey/${userID}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPatientInfo(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching patient information:", error);
+      });
+  };
+
+
+  const [patientVisits, setPatientVisits] = useState(null);
+  const fetchPatientVisits = (userID) => {
+    console.log(userID)
+    fetch(`http://${loginIP}:5000/scheduler/${userID}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPatientVisits(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching patient visits:", error);
+      });
+  };
+
+
   return (
     <div>
       <Flex
@@ -73,6 +123,7 @@ export default function HomePanel() {
         alignItems="baseline"
         justifyContent="left"
         background={isUnauthorizedAccess ? "white" : "blue.400"}
+        overflow= "auto"
       >
         {isUnauthorizedAccess && (
           <Wrap justify="center" mt={10}>
@@ -95,55 +146,76 @@ export default function HomePanel() {
                 <Heading mb={3}>Welcome!</Heading>
               </Stack>
               
-              <Box mt={4}> 
+              <Flex direction="row">
+                <Box mt={4} flex={1}> 
+                  <Flex
+                    width="50vh"
+                    ml={100}
+                    direction="column"
+                    background="blue.100"
+                    p={12}
+                    rounded={6}
+                  >
+                    <Stack direction="row" justify="left">
+                      <Heading size='md'>Upcoming Visits</Heading>
+                    </Stack>
+
+                    {patientVisits && patientVisits.map((patient, index) => (
+                    <Flex 
+                      key={index} 
+                      direction="column" 
+                      background="blue.100" 
+                      p={4} 
+                      rounded={6} 
+                      mt={4}>
+
+                      <Text><b>Start Time:</b> {patient.start_time}</Text>
+                      <Text><b>End Time:</b> {patient.end_time}</Text>
+                      <Text><b>Appointment Details:</b> {patient.context}</Text>
+
+                    </Flex>
+                  ))}
+                  </Flex>
+                </Box>
+              </Flex>
+
+              <Box mt={4} ml={95}> 
                 <Flex
-                  width="50vh"
-                  ml={100}
                   direction="column"
                   background="blue.100"
                   p={12}
                   rounded={6}
                 >
                   <Stack direction="row" justify="left">
-                    <Heading size='md'>Upcoming Visits</Heading>
+                    <Heading size='md'> Patient Information</Heading>
                   </Stack>
 
-                  <Stack direction="column" justify="center">
-                    <Wrap spacing="20px" mt={3}>
-                      <Text fontSize="sm">Dates</Text>
-                    </Wrap>
-                  </Stack>
+                  {patientInfo && patientInfo.map((patient, index) => (
+                    <Flex 
+                      key={index} 
+                      direction="column" 
+                      background="blue.100" 
+                      p={4} 
+                      rounded={6} 
+                      mt={4}>
 
-                </Flex>
-              </Box>
-              
-              <Spacer />
-  
-              <Box mt={4}> 
-                <Flex
-                  width="50vh"
-                  ml={100}
-                  direction="column"
-                  background="blue.100"
-                  p={12}
-                  rounded={6}
-                >
-                  <Stack direction="row" justify="left">
-                    <Heading size='md'>Account Balance</Heading>
-                  </Stack>
+                      <Heading size="md">{patient.fullName}</Heading>
+                      <Text><b>Age:</b> {patient.age}</Text>
+                      <Text><b>Date of Birth:</b> {patient.dob}</Text>
+                      <Text><b>Phone:</b> {patient.phone}</Text>
+                      <Text><b>Address:</b> {patient.address}</Text>
+                      <Text><b>Gender:</b> {patient.gender}</Text>
+                      <Text><b>Medications:</b> {patient.medications}</Text>
+                      <Text><b>Family History:</b> {patient.familyHistory}</Text>
+                      <Text><b>Patient History:</b> {patient.patientHistory}</Text>
 
-                  <Stack direction="column" justify="center">
-                    <Wrap spacing="20px" mt={3}>
-                      <Text fontSize="sm">Amount Due</Text>
-                    </Wrap>
-                  </Stack>
-                
-                </Flex>
-              </Box>
-  
-            </Flex>
-          </Stack>
-        )}
-      </Flex>
-    </div>
-  )};
+                    </Flex>
+                  ))}
+              </Flex>
+            </Box>
+          </Flex>
+        </Stack>
+      )}
+    </Flex>
+  </div>
+)};
